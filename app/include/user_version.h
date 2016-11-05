f2cf1d0610d1067a70df17f724c8b2517574fa19
Andrea Baldini <veon82@gmail.com>
2016-11-05 15:15:34 +0100

    my application settings (tested)

--- app/include/user_version.h
+++ app/include/user_version.h
@@ -6,9 +6,9 @@
 #define NODE_VERSION_REVISION	0U
 #define NODE_VERSION_INTERNAL   0U
 
-#define NODE_VERSION	"NodeMCU 1.4.0"
+#define NODE_VERSION	"NodeMCU 1.4.0-bbhome"
 #ifndef BUILD_DATE
-#define BUILD_DATE	  "20151006"
+#define BUILD_DATE	  "20151105"
 #endif
 
 extern char SDK_VERSION[];
